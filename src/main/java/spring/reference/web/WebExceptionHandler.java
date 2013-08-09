6c67f03b8c989128dd0467027c7fb8768a2e4ed9
shakaihatsu <shakaihatsu@gmail.com>
2013-08-09 17:40:29 +0200

    Added web exception handling through @ControllerAdvice.

--- src/main/java/spring/reference/web/WebExceptionHandler.java
+++ src/main/java/spring/reference/web/WebExceptionHandler.java
@@ -9,13 +9,16 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import spring.reference.exception.RecordNotFoundException;
+import spring.reference.util.Logged;
 
-// @ControllerAdvice
-public abstract class WebExceptionHandler {
+@Logged
+@ControllerAdvice
+public class WebExceptionHandler {
     private static final Logger LOGGER = LoggerFactory.getLogger(WebExceptionHandler.class);
 
     @ExceptionHandler(RecordNotFoundException.class)
