5396cbb344a995687688243c65b90f876ebf6815
shakaihatsu <shakaihatsu@gmail.com>
2013-08-09 12:25:22 +0200

    Added POI in ApplicationPoi regarding the jboss-deployment-structure.xml configuration file.

--- src/main/java/spring/reference/ApplicationPoi.java
+++ src/main/java/spring/reference/ApplicationPoi.java
@@ -20,9 +20,17 @@ public final class ApplicationPoi {
     public static final String DATASOURCE = "admin-ds.xml";
 
     @ExternalDependency
+    @JBOSS_AS7
+    @POI(
+        tags = { POITag.DATASOURCE },
+        value = "A quote from a Spring forum: ''Spring's AOP runtime tries to get information on class 'org.jboss.jca.adapters.jdbc.WrapperDataSource' but cannot load it from WAR's module classloader. The object you (I) have obtained from JNDI is JBoss' DataSource with the class loaded from one of JBoss' modules, but this module isn't available to your (our) WAR. My 'hack' exposes the required class to WAR and Spring's AOP infrastructure (actually AspectJ).''")
+    public static final String DATASOURCE_FOR_SPRING_AOP = "jboss-deployment-structure";
+
+    @ExternalDependency
     @POI(
         tags = { POITag.TRANSACTION, POITag.BE_CAREFUL },
         value = "With JTA, had to set hibernate property 'hibernate.transaction.manager_lookup_class' in the persistence.xml. Probably there's a way to acquire"
             + " an appropriate JTA transaction manager through Spring configuration as well.")
     public static final String TRANSACTION_MANAGER = "";
+
 }
